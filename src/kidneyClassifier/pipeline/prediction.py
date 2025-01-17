30b6c08afce82d3fa9a1fea0c07d24e2985e69c6
Tridev9 <triddev123@gmail.com>
2025-01-17 21:52:11 +0530

    prediction added

---
+++ src/kidneyClassifier/pipeline/prediction.py
@@ -0,0 +1,38 @@
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import os
+
+
+
+class PredictionPipeline:
+    def __init__(self,filename):
+        self.filename =filename
+
+
+    
+    def predict(self):
+        # load model
+         # Get the absolute path to the model file
+        model_path = os.path.join("artifacts", "training", "model.h5")
+        
+        # Check if model exists
+        if not os.path.exists(model_path):
+            raise FileNotFoundError(f"Model not found at {model_path}")
+            
+        # load model
+        model = load_model(model_path)
+
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size = (224,224))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = np.argmax(model.predict(test_image), axis=1)
+        print(result)
+
+        if result[0] == 1:
+            prediction = 'Tumor'
+            return [{ "image" : prediction}]
+        else:
+            prediction = 'Normal'
+            return [{ "image" : prediction}] 
