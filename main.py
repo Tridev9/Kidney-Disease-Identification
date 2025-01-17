766c9f982e043380faa1d60d08317bda8ff2f6a0
Tridev9 <triddev123@gmail.com>
2025-01-17 12:44:21 +0530

    base model created

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kidneyClassifier import logger
 from kidneyClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from kidneyClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 
@@ -16,3 +17,13 @@ except Exception as e:
 
 
 
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
